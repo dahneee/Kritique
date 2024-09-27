d3b54127309371cea866c6ef96bd1b5e321c7314
dahneee <daniellemariedeguzman@gmail.com>
2024-09-28 01:02:11 +0800

    routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('/admin/dashboard', function () {
+    return view('admin-dashboard');
+});
