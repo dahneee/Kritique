d05dbf94f0982d4ac9341b2376c7e4cdd81b76e1
genesistgl <tglgenesis@gmail.com>
2024-09-28 08:44:59 +0800

    sidebar-admin_dashb-routes

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,25 @@ Route::get('/', function () {
     return view('login');
 });
 
+Route::get('/admin/db', function () {
+    return view('admin-db');
+});
+
 Route::get('/admin/dashboard', function () {
     return view('admin-dashboard');
 });
+
+Route::get('/user/dashboard', function () {
+    return view('user-dashboard');
+});
+
+Route::get('/eval', function () {
+    return view('eval-form');
+});
+
+
+Route::get('/eval', function () {
+    return view('eval-form');
+});
+
+
