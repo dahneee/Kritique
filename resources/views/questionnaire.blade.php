3ecbf438558c6f697bcf0cbf8d06287b52633cd5
Yurei <glba.alicoben.up@phinmaed.com>
2024-10-10 21:53:32 +0800

    hsaisuhais

--- resources/views/questionnaire.blade.php
+++ resources/views/questionnaire.blade.php
@@ -29,7 +29,7 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="zaiModelLabel">Add Your Questions (Max 200):</h5>
+                            <h5 class="modal-title" id="zaiModelLabel">Add Your Questions (Max 10):</h5>
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
