0ab8c2335089bc1a1cfa29c20be8faad0ac7032b
dahneee <daniellemariedeguzman@gmail.com>
2024-10-16 14:28:28 +0800

    seeder and view

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,18 +2,21 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
-use App\Models\User;
 use App\Models\Department;
 use App\Models\Block;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Faker\Factory as Faker;
 
 class UserSeeder extends Seeder
 {
     public function run()
     {
-        $departments = Department::pluck('department_id')->toArray();
-        $blocks = Block::pluck('block_id')->toArray();
+        $faker = Faker::create();
+
+        $departments = Department::all();
+        $blocks = Block::all();
 
         $adminUser = [
             'first_name' => 'admin',
@@ -29,70 +32,18 @@ class UserSeeder extends Seeder
             User::create($adminUser);
         }
 
-        $users = [
-            [
-                'student_id' => '03-2021',
-                'first_name' => 'John',
-                'middle_name' => 'A',
-                'last_name' => 'Doe',
-                'block' => '',
-                'department' => '',
-                'email' => 'john@example.com',
+        foreach (range(1, 50) as $index) {
+            $student = User::create([
+                'student_id' => $faker->unique()->numerify('##-####'),
+                'first_name' => $faker->firstName,
+                'middle_name' => $faker->firstName,
+                'last_name' => $faker->lastName,
+                'email' => $faker->unique()->email,
                 'password' => Hash::make('password123'),
-            ],
-            [
-                'student_id' => '02-2023',
-                'first_name' => 'Jane',
-                'middle_name' => 'B',
-                'last_name' => 'Smith',
-                'block' => '',
-                'department' => '',
-                'email' => 'jane@example.com',
-                'password' => Hash::make('password123'),
-            ],
-            [
-                'student_id' => '03-2023',
-                'first_name' => 'Emily',
-                'middle_name' => 'C',
-                'last_name' => 'Johnson',
-                'block' => '',
-                'department' => '',
-                'email' => 'emily@example.com',
-                'password' => Hash::make('password123'),
-            ],
-            [
-                'student_id' => '04-2023',
-                'first_name' => 'Michael',
-                'middle_name' => 'D',
-                'last_name' => 'Williams',
-                'block' => '',
-                'department' => '',
-                'email' => 'michael@example.com',
-                'password' => Hash::make('password123'),
-            ],
-            [
-                'student_id' => '05-2023',
-                'first_name' => 'Jessica',
-                'middle_name' => 'E',
-                'last_name' => 'Brown',
-                'block' => '',
-                'department' => '',
-                'email' => 'jessica@example.com',
-                'password' => Hash::make('password123'),
-            ],
-        ];
-
-        foreach ($users as &$user) {
-            if (isset($user['student_id'])) {
-                $user['block'] = $blocks[array_rand($blocks)];
-                $user['department'] = $departments[array_rand($departments)];
-            }
-
-            if (!User::where('email', $user['email'])->exists()) {
-                if (isset($user['department']) && !empty($user['department'])) {
-                    User::create($user);
-                }
-            }
+                'block' => $blocks->random()->block_id, // Assign random block
+                'department' => $departments->random()->department_id, // Assign random department
+                'user_type' => 'student',
+            ]);
         }
     }
 }
