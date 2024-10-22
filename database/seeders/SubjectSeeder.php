1bef2eb012cec264ef2a08b3f588d43910e48218
dahneee <daniellemariedeguzman@gmail.com>
2024-10-22 14:03:08 +0800

    added subjects in SubjectSseeder

--- database/seeders/SubjectSeeder.php
+++ database/seeders/SubjectSeeder.php
@@ -34,6 +34,42 @@ class SubjectSeeder extends Seeder
                 'subject_id' => 'SSP 007',
                 'subject_name' => 'Student Success Program 3',
             ],
+            [
+                'subject_id' => 'ITE 260',
+                'subject_name' => 'Computer Programming 1',
+            ],
+            [
+                'subject_id' => 'MAT 152',
+                'subject_name' => 'Mathematics in the Modern World',
+            ],
+            [
+                'subject_id' => 'PED 025',
+                'subject_name' => 'Movement Enhancement',
+            ],
+            [
+                'subject_id' => 'NST 022',
+                'subject_name' => 'National Service Training Program 2',
+            ],
+            [
+                'subject_id' => 'GEN 005',
+                'subject_name' => 'The Contemporary World',
+            ],
+            [
+                'subject_id' => 'GEN 004',
+                'subject_name' => 'Readings in Philippine History',
+            ],
+            [
+                'subject_id' => 'CHE 025',
+                'subject_name' => 'Chemistry',
+            ],
+            [
+                'subject_id' => 'BES 025',
+                'subject_name' => 'Statics of Rigid Bodies',
+            ],
+            [
+                'subject_id' => 'BES 043',
+                'subject_name' => 'Computer Programming (Eng)',
+            ],
         ];
 
         foreach ($subjects as $subject) {
